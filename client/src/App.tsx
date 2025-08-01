09dd0d98c03943f1f721d140c148395aea20c940
rb-cesar <rbcesar86@gmail.com>
2025-08-01 13:16:40 -0300

    absolute import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
+import Card from '@/src/components/Card';
+import Text from '@/src/components/Text';
+
 export default function App() {
   return (
     <div>
       <h1>Hello SSR with React + Vite + Express!</h1>
-      {/* <Card> */}
-      {/* <Text>Apenas um teste</Text> */}
-      {/* </Card> */}
+      <Card>
+        <Text>Apenas um teste</Text>
+      </Card>
     </div>
   );
 }
